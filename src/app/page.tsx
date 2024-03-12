1f428e599ba959ac6477b0b8b072d91320439fa7
AndrewTake <andrew6041@gmail.com>
2024-03-11 18:59:33 -0700

    new css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,15 @@
 
 // export default function Home() {
 //   return (
-//     <main className=""> Reference.
+//     <main className=""> Reference
 //     </main>
 //   );
 // }
 import Image from "next/image";
-import styles from './Home.module.css'; // Make sure to create this CSS module file
 
 export default function Home() {
   return (
-    <main className={styles.centeredContent}>
+    <main className="centeredContent">
       <div>
         <h1>Reference</h1>
         <p>Andrew Hull Charlie Dixon</p>
