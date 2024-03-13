d10975c6d3122d291fa901ffbb0616f719f3bfef
AndrewTake <andrew6041@gmail.com>
2024-03-12 23:29:17 -0700

    remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@
 // }
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="centeredContent">
-      <div>
-        <h1>Reference</h1>
-        <p>Andrew Hull Charlie Dixon</p>
-      </div>
-    </main>
-  );
-}
+// export default function Home() {
+//   return (
+//     <main className="centeredContent">
+//       <div>
+//         <h1>Reference</h1>
+//         <p>Andrew Hull Charlie Dixon</p>
+//       </div>
+//     </main>
+//   );
+// }
