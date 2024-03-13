c97482a0f30e72b85127b204c6827db1aa6d18c2
AndrewTake <andrew6041@gmail.com>
2024-03-13 00:18:23 -0700

    final push of night, added Paragraph, added test function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@
 //   );
 // }
 import Image from "next/image";
+import Paragraph from "./components/ui/paragraph";
 
 export default function Home() {
   return (
     <main className="centeredContent">
+      <Paragraph size="sm">This is a test</Paragraph>
       <div>
         <h1>Reference</h1>
         <p>Andrew Hull Charlie Dixon</p>
