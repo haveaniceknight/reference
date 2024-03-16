8740004e0d8d5bac0b6f3af3a4ec3eaf9d79612d
AndrewTake <andrew6041@gmail.com>
2024-03-15 19:44:18 -0700

    new

--- src/components/sigin.tsx
+++ src/components/sigin.tsx
@@ -1,38 +1,38 @@
-'use client'
+// 'use client'
 
-import { signIn } from 'next-auth/react'
-import { FC, useState } from 'react'
-import { Button } from '@/Button'
-// import { toast } from './toast'
+// import { signIn } from 'next-auth/react'
+// import { FC, useState } from 'react'
+// import { Button } from '@/Button'
+// // import { toast } from './toast'
 
-/**
- * NextJS does not allow to pass function from server -> client components,
- * hence this unreusable component.
- */
+// /**
+//  * NextJS does not allow to pass function from server -> client components,
+//  * hence this unreusable component.
+//  */
 
-interface SignInButtonProps { }
+// interface SignInButtonProps { }
 
-const SignInButton: FC<SignInButtonProps> = ({ }) => {
-    const [isLoading, setIsLoading] = useState<boolean>(false)
+// const SignInButton: FC<SignInButtonProps> = ({ }) => {
+//     const [isLoading, setIsLoading] = useState<boolean>(false)
 
-    const signInWithGoogle = async () => {
-        try {
-            setIsLoading(true)
-            await signIn('google')
-        } catch (error) {
-            // toast({
-            //     title: 'Error signing in',
-            //     message: 'Please try again later.',
-            //     type: 'error',
-            // })
-        }
-    }
+//     const signInWithGoogle = async () => {
+//         try {
+//             setIsLoading(true)
+//             await signIn('google')
+//         } catch (error) {
+//             // toast({
+//             //     title: 'Error signing in',
+//             //     message: 'Please try again later.',
+//             //     type: 'error',
+//             // })
+//         }
+//     }
 
-    return (
-        <Button onClick={signInWithGoogle} isLoading={isLoading}>
-            Sign in
-        </Button>
-    )
-}
+//     return (
+//         <Button onClick={signInWithGoogle} isLoading={isLoading}>
+//             Sign in
+//         </Button>
+//     )
+// }
 
-export default SignInButton
+// export default SignInButton
