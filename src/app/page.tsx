001cb8bb85f8a2a168354be2499e4c114fff727d
AndrewTake <andrew6041@gmail.com>
2024-03-15 20:02:27 -0700

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import Paragraph from "@/components/ui/paragraph";
 
 export default function Home() {
   return (
     <main className="centeredContent">
-      <Paragraph size="sm"></Paragraph>
       <div>
         <h1>Reference</h1>
         <p>Andrew Hull Charlie Dixon</p>
