a302ac85cd29718985e9fec5cfa0afd7627a52bf
haveaniceknight <andrewxcoder@gmail.com>
2024-03-24 14:44:27 -0700

    New message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,1 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="centeredContent">
-      <div>
-        <h1>Reference</h1>
-        <p>Andrew Hull Charlie Dixon</p>
-      </div>
-    </main>
-  );
-}
