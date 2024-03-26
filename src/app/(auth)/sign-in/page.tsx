97119d24546e3bf63937d8dcaafab6e3a2cce7ec
haveaniceknight <andrewxcoder@gmail.com>
2024-03-26 13:11:43 -0700

    sign in and user agreement added

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,24 @@
+import { buttonVariants } from '@/components/ui/Button'
+import { FC } from 'react'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import SignIn from '@/components/ui/SignIn'
+
+interface pageProps {
+
+}
+
+const page: FC<pageProps> = ({ }) => {
+    return <div className='absolute inset-0'>
+        <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
+            <Link
+                href='/'
+                className={cn(buttonVariants({ variant: 'ghost' }),
+                    'self-start -mt-20')}>Home</Link>
+            <SignIn />
+        </div>
+
+        page</div>
+}
+
+export default page
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { Icons } from "./Icons"
+import { buttonVariants } from "./Button"
 
 const Navbar = () => {
     return <div className='fixed top-2 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-5' >
@@ -9,6 +10,9 @@ const Navbar = () => {
                 {/* <p className="hidden text-zinc-700 text-sm font-medium md:block">Reference</p> */}
                 <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6"></Icons.logo>
             </Link>
+            {/* search bar */}
+            <Link href='/sign-in' className={buttonVariants()}>Sign In</Link>
+
 
         </div>
     </div>
