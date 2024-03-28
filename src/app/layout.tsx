6efc42c251a3e4406e14be64646253feb775d664
haveaniceknight <andrewxcoder@gmail.com>
2024-03-27 18:06:26 -0700

    please pass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     //appending inter classname with cn, to bg classname
     <html lang='en' className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-
+        {/* @ts-expect-error server component*/}
         <Navbar />
         <div className='container max-w-7xl mx-auto h-full pt-12'></div>
 
